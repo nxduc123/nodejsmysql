22c1442b3d509f2b9a94f2ecbb9dd165070d3cb6
Your Name <nxduc123@gmail.com>
2018-04-15 23:32:00 +0700

    sdasdsa

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var bodyparser = require('body-parser');
 var connection = require('./connection');
 var routes = require('./routes');
 var morgan = require('morgan');
-  
+var port = process.env.PORT || 3000;
 var app = express();
 app.use(bodyparser.urlencoded({extended: true}));
 app.use(bodyparser.json());
@@ -12,7 +12,7 @@ app.use(bodyparser.json());
 app.use(morgan('dev'));
 connection.init();
 routes.configure(app);
-var server = app.listen(process.env || 5000, function() {
+var server = app.listen(port, function() {
     
   console.log('Server listening on port ' + server.address().port);
 });
